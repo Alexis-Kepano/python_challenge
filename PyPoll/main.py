b3a4b5c7ef46aa929458b12cf025fd64856fc6f2
Alexis Kepano <aakepano@gmail.com>
2021-01-14 16:15:08 -0500

    python commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,36 +5,28 @@ import csv
 csvpath=os.path.join('Resources', 'election_data.csv')
 #output
 outfile = os.path.join('Analysis', 'pypoll.txt')
+#declare variables   
+totalvotes = 0; kvotes = 0; ccount = 0; lcount = 0; ocount = 0; maxvotecount = 0
 
 with open(csvpath) as csvfile:
     header = csvfile.readline()
     rows = csv.reader(csvfile)
 
-for i in csvreader:
-    voterid = i[0]
-    county = i[1]
-    candidate = i[2] 
-
- #declare variables   
-totalvotes = 0; kvotes = 0; ccount = 0; lcount = 0; ocount = 0; maxvotecount = 0
-
- #Total Vote Count
-totalcount = totalcount + 1
-
-#OR total vote count
-totalcount = (len(votes))
-print(totalcount)
-
 #votes by candidate
-for candidate in candidates:
-    if candidate =="Khan":
-     kcount = kcount + 1
-    if candidate =="Correy":
-     ccount = ccount + 1
-    if candidate =="Li":
-     lcount = lcount + 1
-    if candidate =="O'Tooley":
-     ocount = ocount + 1
+    for candidate in candidates:
+
+        #total vote count
+        totalcount = (len(votes))
+        print(totalcount)
+
+        if candidate =="Khan":
+            kcount = kcount + 1
+        if candidate =="Correy":
+            ccount = ccount + 1
+        if candidate =="Li":
+            lcount = lcount + 1
+        if candidate =="O'Tooley":
+            ocount = ocount + 1
 
 # Define (dictionary) list : candidate and votes
 candidatevote = {"Khan": kcount,"Correy": ccount,"Li" :lcount, "O'Tooley": ocount}
@@ -45,6 +37,25 @@ for candidate, value in candidatevote.items():
     winner = candidate
 
 #Output
+output = f"""
+Election Results
+-------------------------
+Total Votes: {totalVotes}
+-------------------------
+Khan: {(Khan_votes/totalVotes)*100:.3f}%  ({Khan_votes})
+Correy: {(Correy_votes/totalVotes)*100:.3f}% ({Correy_votes})
+Li: {(Li_votes/totalVotes)*100:.3f}%  ({Li_votes})
+O'Tooley: {(OTooley_votes/totalVotes)*100:.3f}% ({OTooley_votes})
+-------------------------
+Winner: {winner}
+-------------------------
+"""
+
+print(output)
+
+with open(outfile, 'w') as outputFile:
+    outputFile.write(output)
+
 
 
 
